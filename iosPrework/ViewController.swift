60b00ad73589306df3167bf71b05849b57a4d512
Tatum Allen <82176414+tatumalllen@users.noreply.github.com>
2022-01-13 21:25:33 -0700

    This is a code for a very basic IOS tip calculator app using Swift. This was my very first time making a project using this language.

--- iosPrework/ViewController.swift
+++ iosPrework/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "Tip Calculator"
     }
 
-
+    @IBAction func calculateTip(_ sender: Any)
+    {
+        //get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        //get total by multiplying * tip percentages
+        let tipPercentages = [0.15, 0.18, 0.20]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        //Update Total Amount
+        totalLabel.text = String(format: "$%.2f", tip)
+        //Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
 }
 
